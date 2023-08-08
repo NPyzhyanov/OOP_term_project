6eef5317b23f8f46e5a80e55c318b6b0885b980a
NPyzhyanov <pyzhyanov_n@aol.com>
2023-08-08 22:10:49 +0300

    Initial commit (+sln)

--- Racing_Simulator/main.cpp
+++ Racing_Simulator/main.cpp
@@ -26,6 +26,8 @@ int main()
             std::cout << ++l_counter << ". " << current_word << std::endl;
         }
     }
+
+    const int land_vcls_amount = l_counter;
     
     std::cout << "\nAir vehicles: " << std:: endl;
     
@@ -44,57 +46,63 @@ int main()
             std::cout << ++a_counter << ". " << current_word << std::endl;
         }
     }
+
+    const int air_vcls_amount = a_counter;
     
     std::cout << std::endl;
     
-    vehicle::Vehicle* p_land_vehicles_array[l_counter];
-    for (int i = 0; i < l_counter; i++)
+    vehicle::Vehicle** p_land_vehicles_array = new vehicle::Vehicle*[land_vcls_amount];
+    for (int i = 0; i < land_vcls_amount; i++)
     {
         p_land_vehicles_array[i] = vehicle::create_vehicle(p_l_list[i]);
         std::cout << p_land_vehicles_array[i]->get_instance_name() << " created" << std::endl;
     }
     
-    vehicle::Vehicle* p_air_vehicles_array[a_counter];
-    for (int i = 0; i < a_counter; i++)
+    vehicle::Vehicle** p_air_vehicles_array = new vehicle::Vehicle*[air_vcls_amount];
+    for (int i = 0; i < air_vcls_amount; i++)
     {
         p_air_vehicles_array[i] = vehicle::create_vehicle(p_a_list[i]);
         std::cout << p_air_vehicles_array[i]->get_instance_name() << " created" << std::endl;
     }
     
-    double land_vcls_results[l_counter];
+    double* land_vcls_results = new double[land_vcls_amount];
     
-    for (int i = 0; i < l_counter; i++)
+    for (int i = 0; i < land_vcls_amount; i++)
     {
         land_vcls_results[i] = p_land_vehicles_array[i]->ready_set_go(DISTANCE);
     }
     
-    double air_vcls_results[a_counter];
+    double* air_vcls_results = new double[air_vcls_amount];
     
-    for (int i = 0; i < a_counter; i++)
+    for (int i = 0; i < air_vcls_amount; i++)
     {
         air_vcls_results[i] = p_air_vehicles_array[i]->ready_set_go(DISTANCE);
     }
     
     std::cout << "\nRESULTS\n\n";
     
-    for (int i = 0; i < l_counter; i++)
+    for (int i = 0; i < land_vcls_amount; i++)
     {
         std::cout << p_land_vehicles_array[i]->get_instance_name() << "\t\t" << land_vcls_results[i] << std::endl;
     }
-    for (int i = 0; i < a_counter; i++)
+    for (int i = 0; i < air_vcls_amount; i++)
     {
         std::cout << p_air_vehicles_array[i]->get_instance_name() << "\t\t" << air_vcls_results[i] << std::endl;
     }
     
-    for (int i = 0; i < l_counter; i++)
+    for (int i = 0; i < land_vcls_amount; i++)
     {
         vehicle::delete_vehicle(p_land_vehicles_array[i]);
     }
-    for (int i = 0; i < a_counter; i++)
+    for (int i = 0; i < air_vcls_amount; i++)
     {
         vehicle::delete_vehicle(p_air_vehicles_array[i]);
     }
     
+    delete[] land_vcls_results;
+    delete[] air_vcls_results;
+    delete[] p_land_vehicles_array;
+    delete[] p_air_vehicles_array;
     vehicle::delete_list(p_l_list);
     vehicle::delete_list(p_a_list);
     
